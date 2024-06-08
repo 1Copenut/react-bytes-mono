bfc83316f0571db963b51a44337f19ead3c5b914
1Copenut <pierce.trevor@gmail.com>
2024-06-08 13:34:18 -0500

    First commit Auth0 React Typescript app.

---
+++ packages/auth/auth0-react/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <p>This will be where I put more interesting content. Soon.</p>
+    </>
+  );
+};
+
+export default App;
